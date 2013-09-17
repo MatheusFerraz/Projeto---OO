eaf6a5a897f9b35e6a7f7bd2073996d620b59735
Matheus Ferraz <matheus_ccmf@yahoo.com.br>
2013-09-17 20:10:15 -0300

    Add Classe Principal

---
+++ Principal.java
@@ -0,0 +1,36 @@
+package hotelaria.func;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		int escolha;
+		
+		System.out.println("Bem-vindo ao gerenciador de Hotel");
+		System.out.println("Escolha um opção abaixo");
+		System.out.println("1-Cadastrar Novo Hotel.\n2-Cadastrar funcionario.\n3-Cadastrar APartamento.\n4-Cadastrar Cliente.\n");
+		
+		escolha = input.nextInt();
+		
+		switch(escolha){
+		case 1:
+			Hotel myHotel = new Hotel();
+			myHotel.cadastrarHotel();
+			System.out.println(myHotel.getNome());
+			break;
+		case 2:
+			break;
+		case 3:
+			break;	
+		case 4:
+			break;
+		default:
+			break;
+		}
+	}
+}
+	

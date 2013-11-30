988cd1b22f37878ece38eea381b7b5d3ee713092
Matheus Ferraz <matheus_ccmf@yahoo.com.br>
2013-11-30 21:07:16 -0200

    Add all java files

--- ConexaoBancoDados.java
+++ ConexaoBancoDados.java
@@ -1,14 +1,8 @@
 package hotelaria.func;
 
-import java.sql.Time;
-import java.util.Date;
-
 public class ConexaoBancoDados {
 	
 	//Especificando atributos para a Classe
-	private static String tipoConexaoBD;
-	private static Date dataConexaoBD;
-	private static Time tempoConexaoBD;
 	private static boolean statusConexaoBD;
 	
 	//Construtor SEM PARÂMETROS
@@ -17,31 +11,6 @@ public class ConexaoBancoDados {
 	}
 	
 	//Métodos de Acesso e Modificação
-
-	public static String getTipoConexaoBD() {
-		return tipoConexaoBD;
-	}
-
-	public static void setTipoConexaoBD(String tipoConexaoBD) {
-		ConexaoBancoDados.tipoConexaoBD = tipoConexaoBD;
-	}
-
-	public static Date getDataConexaoBD() {
-		return dataConexaoBD;
-	}
-
-	public static void setDataConexaoBD(Date dataConexaoBD) {
-		ConexaoBancoDados.dataConexaoBD = dataConexaoBD;
-	}
-
-	public static Time getTempoConexaoBD() {
-		return tempoConexaoBD;
-	}
-
-	public static void setTempoConexaoBD(Time tempoConexaoBD) {
-		ConexaoBancoDados.tempoConexaoBD = tempoConexaoBD;
-	}
-
 	public static boolean isStatusConexaoBD() {
 		return statusConexaoBD;
 	}
